22dc3668d3da17256786d8168a846a1f1a69df82
emmanueluc322 <61635797+emmanueluc322@users.noreply.github.com>
2025-12-22 22:53:59 -0500

    AUTH.....firebase

--- packages/user_repository/lib/src/firebase_user_repo.dart
+++ packages/user_repository/lib/src/firebase_user_repo.dart
@@ -1,1 +1,9 @@
-class F
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/user_repo.dart';
+
+class FirebaseUserRepo implements UserRepository {
+  final FirebaseAuth _firebaseAuth
+  final userCollection = FirebaseFirestore.instance.collection('users');
+
+}
